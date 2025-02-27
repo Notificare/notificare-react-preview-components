9e2d4cc4fd00b417faeb9a1069589f04bf075ea7
rubensantos <rubensantos994@hotmail.com>
2025-02-27 17:33:57 +0000

    NPC-16 fix: added a delay implementation between webshot requests to avoid API overload

--- src/components/shared/webshot/Webshot.tsx
+++ src/components/shared/webshot/Webshot.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import './Webshot.css';
-import CONFIG from '../../../../config';
+import { CONFIG } from '../../../../config';
+import { isValidUrl } from '../../../helpers/isValidUrl';
 import LoadingIcon from '../loading-icon/LoadingIcon';
 import PreviewError from '../preview-error/PreviewError';
 
@@ -36,24 +37,43 @@ async function getWebshot(url: string, width: number, height: number, platform: 
 export default function Webshot(props: WebshotProps) {
   const { url, platform, width, height, onLoadingChange, canShow = true } = props;
 
+  const [webshot, setWebshot] = useState('');
   const [hasError, setHasError] = useState(false);
-  const [pageImage, setPageImage] = useState('');
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [lastRun, setLastRun] = useState(Date.now());
+
+  const extraTimeToWrite = 3000;
+  const delayBetweenRequests = 15000;
 
   useEffect(() => {
-    (async () => {
-      setIsLoading(true);
+    setIsLoading(true);
+    onLoadingChange?.(true);
+    setHasError(false);
 
-      const image = await getWebshot(url, width, height, platform);
-      setPageImage(image);
+    if (!isValidUrl(url)) {
+      setHasError(true);
+      setIsLoading(false);
+      onLoadingChange?.(false);
+      return;
+    }
 
-      if (!image) {
+    const timeSinceLastRun = Date.now() - lastRun;
+    const remainingTime = delayBetweenRequests - timeSinceLastRun;
+
+    const handler = setTimeout(async () => {
+      setLastRun(Date.now());
+      const webshot = await getWebshot(url, width, height, platform);
+      setWebshot(webshot);
+
+      if (!webshot) {
         setHasError(true);
       }
 
       setIsLoading(false);
       onLoadingChange?.(false);
-    })();
+    }, remainingTime + extraTimeToWrite);
+
+    return () => clearTimeout(handler);
   }, [url]);
 
   return (
@@ -63,7 +83,7 @@ export default function Webshot(props: WebshotProps) {
       ) : isLoading || !canShow ? (
         <LoadingIcon />
       ) : (
-        <img src={`data:image/png;base64,${pageImage}`} alt="Webshot" />
+        <img src={`data:image/png;base64,${webshot}`} alt="Webshot" />
       )}
     </div>
   );
