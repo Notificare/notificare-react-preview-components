77927cf72965aad53296684f129a2c3fe65476c4
rubensantos <rubensantos994@hotmail.com>
2025-03-18 12:24:05 +0000

    NPC-5 NPC-6 NPC-8 NPC-9 refactor: combination of ios/android lock screen previews in single components

--- src/components/previews/android-lock-screen-push/AndroidLockScreenPush.tsx
+++ src/components/previews/android-lock-screen-push/AndroidLockScreenPush.tsx
@@ -5,15 +5,19 @@ import { BasePushMessage } from '../../../schemas/basePushMessageSchema';
 import AndroidAppIcon from '../../shared/android-app-icon/AndroidAppIcon';
 import AndroidPhoneBackground from '../../shared/android-phone-background/AndroidPhoneBackground';
 
-interface AndroidLockScreenPushMessageProps {
+export interface AndroidLockScreenPushProps {
   notification: BasePushMessage;
   appName: string;
   appIcon: string;
+  expanded: boolean;
 }
 
-export default function AndroidLockScreenPush(props: AndroidLockScreenPushMessageProps) {
-  const { notification, appName, appIcon } = props;
-
+export default function AndroidLockScreenPush({
+  notification,
+  appName,
+  appIcon,
+  expanded,
+}: AndroidLockScreenPushProps) {
   return (
     <div className="notificare">
       <AndroidPhoneBackground theme="light">
@@ -23,10 +27,10 @@ export default function AndroidLockScreenPush(props: AndroidLockScreenPushMessag
             <div className="notificare__android-lock-screen-push-text-content">
               <div className="notificare__android-lock-screen-push-text-content-top">
                 <div
-                  className={`notificare__android-lock-screen-push-title-and-subtitle ${hasFirstAttachment(notification) && 'notificare__android-lock-screen-push-title-and-subtitle--has-media'}`}
+                  className={`notificare__android-lock-screen-push-title-and-subtitle ${hasFirstAttachment(notification) && !expanded && 'notificare__android-lock-screen-push-title-and-subtitle--has-media'}`}
                 >
                   <p className="notificare__android-lock-screen-push-text notificare__android-lock-screen-push-text--title">
-                    {notification.title ? notification.title : appName}
+                    {!expanded ? notification.title || appName : appName}
                   </p>
                   {notification.subtitle && !hasFirstAttachment(notification) && (
                     <p className="notificare__android-lock-screen-push-text notificare__android-lock-screen-push-text--subtitle">
@@ -38,14 +42,31 @@ export default function AndroidLockScreenPush(props: AndroidLockScreenPushMessag
                   now
                 </p>
               </div>
+
+              {expanded && (
+                <p className="notificare__android-lock-screen-push-text notificare__android-lock-screen-push-text--expanded-title">
+                  {notification.title}
+                </p>
+              )}
+
               <p
                 className={`notificare__android-lock-screen-push-text ${hasFirstAttachment(notification) ? 'notificare__android-lock-screen-push-text--message-with-media' : 'notificare__android-lock-screen-push-text--message-without-media'}`}
               >
                 {notification.message}
               </p>
+
+              {hasFirstAttachment(notification) && expanded && (
+                <div className="notificare__android-lock-screen-push-expanded-media">
+                  <img
+                    className="notificare__android-lock-screen-push-expanded-media-image"
+                    src={notification.attachments?.[0].uri}
+                    alt="Expanded media"
+                  />
+                </div>
+              )}
             </div>
 
-            {hasFirstAttachment(notification) && (
+            {hasFirstAttachment(notification) && !expanded && (
               <div className="notificare__android-lock-screen-push-media">
                 <img
                   className="notificare__android-lock-screen-push-media-image"
