05d4bc8c2206e92392a3cbc1d25d509312e98998
rubensantos <rubensantos994@hotmail.com>
2025-04-03 16:35:19 +0100

    NPC-13 feat: rollup bundling configuration

---
+++ rollup.config.mjs
@@ -0,0 +1,47 @@
+import { createRequire } from 'module';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+
+const require = createRequire(import.meta.url);
+const packageJson = require('./package.json');
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({
+        tsconfig: './tsconfig.json',
+        exclude: ['**/*.test.ts', '!**/*.test.tsx', '**/*.stories.ts'],
+      }),
+      terser(),
+      postcss({ extensions: ['.css'], inject: true, extract: false }),
+    ],
+    external: ['react'],
+  },
+  {
+    input: 'src/index.ts',
+    output: [{ file: 'dist/types.d.ts', format: 'esm' }],
+    plugins: [dts()],
+    external: [/\.css$/],
+  },
+];
