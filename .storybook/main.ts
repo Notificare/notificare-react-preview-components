438622ed1d5d38ba1e59648c572331615ecb01e9
rubensantos <rubensantos994@hotmail.com>
2025-06-03 17:35:17 +0100

    chore: order stories alphabetically; removed unnecessary storybook addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,7 +5,6 @@ import svgr from 'vite-plugin-svgr';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@storybook/addon-onboarding',
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
