7d21355df802b115fa52c035f4f596b87c299488
Helder Pinhal <helder@notifica.re>
2025-06-05 17:53:14 +0100

    build: fix rollup build

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,5 +1,5 @@
 import { pathsToModuleNameMapper } from 'ts-jest';
-import tsconfig from './tsconfig.json' with { type: "json" };
+import tsconfig from './tsconfig.json' with { type: 'json' };
 
 export default {
   testEnvironment: 'jsdom',
