be2d5f8e3a325520e9574dbba4abf41ea734b3b9
rubensantos <rubensantos994@hotmail.com>
2025-06-23 14:57:56 +0100

    refactor: replacement of ClickOutside component with useOutsideClick hook with improvements

--- src/internal/components/shared/Selector/Selector.tsx
+++ src/internal/components/shared/Selector/Selector.tsx
@@ -1,8 +1,8 @@
 import { Key, useRef, useState } from 'react';
 import ExpandIcon from '~/assets/expand.svg';
-import { ClickOutside } from '~/internal/components/shared/ClickOutside/ClickOutside';
 
 import './Selector.css';
+import { useOutsideClick } from '~/internal/hooks/click-outside';
 
 export function Selector<T extends Key>({
   label,
@@ -12,41 +12,46 @@ export function Selector<T extends Key>({
   disabled = false,
 }: SelectorProps<T>) {
   const [expanded, setExpanded] = useState(false);
+
   const optionsRef = useRef<HTMLDivElement>(null);
   const selectorButtonRef = useRef<HTMLButtonElement>(null);
+
+  useOutsideClick({
+    refs: [optionsRef, selectorButtonRef],
+    onClickOutside: () => setExpanded(false),
+  });
 
   return (
     <div className="notificare__selector__wrapper">
       <p className="notificare__selector__label">{label}</p>
-      <ClickOutside onClickOutside={() => setExpanded(false)}>
-        <button
-          className="notificare__selector__button"
-          onClick={() => setExpanded(!expanded)}
-          disabled={disabled}
-          ref={selectorButtonRef}
-        >
-          {options.find((option) => option.value === value)?.label}
-          <ExpandIcon className="notificare__selector__button-expand-icon" />
-        </button>
 
-        {expanded && (
-          <div className="notificare__selector__options" ref={optionsRef}>
-            {options.map((option) => (
-              <button
-                key={option.value}
-                className={`notificare__selector__option-button ${value === option.value ? 'notificare__push__preview-controls-selector-option-button--selected' : ''}`}
-                onClick={() => {
-                  onValueChanged?.(option.value);
-                  setExpanded(false);
-                }}
-                data-testid={`selector-option-${option.value}`}
-              >
-                {option.label}
-              </button>
-            ))}
-          </div>
-        )}
-      </ClickOutside>
+      <button
+        className="notificare__selector__button"
+        onClick={() => setExpanded(!expanded)}
+        disabled={disabled}
+        ref={selectorButtonRef}
+      >
+        {options.find((option) => option.value === value)?.label}
+        <ExpandIcon className="notificare__selector__button-expand-icon" />
+      </button>
+
+      {expanded && (
+        <div className="notificare__selector__options" ref={optionsRef}>
+          {options.map((option) => (
+            <button
+              key={option.value}
+              className={`notificare__selector__option-button ${value === option.value ? 'notificare__push__preview-controls-selector-option-button--selected' : ''}`}
+              onClick={() => {
+                onValueChanged?.(option.value);
+                setExpanded(false);
+              }}
+              data-testid={`selector-option-${option.value}`}
+            >
+              {option.label}
+            </button>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
