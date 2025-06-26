2a62344ea03e3bde2ff198ba1c551ceb7eedad80
rubensantos <rubensantos994@hotmail.com>
2025-06-26 14:29:20 +0100

    fix: changing country and adding x parameter to itunes URL

--- src/internal/components/push/platforms/ios/app-ui/AppRecommendationNotification/requests/ios-store-app.ts
+++ src/internal/components/push/platforms/ios/app-ui/AppRecommendationNotification/requests/ios-store-app.ts
@@ -6,8 +6,11 @@ import { NetworkRequestError } from '~/internal/network/errors';
 
 export async function fetchIosStoreApp(appId: string): Promise<IosStoreAppData> {
   const url = new URL('/lookup', 'https://itunes.apple.com');
-  url.searchParams.set('country', 'GB');
+  url.searchParams.set('country', 'NL');
   url.searchParams.set('id', appId);
+
+  const timestamp = (Date.now() / 1000).toString();
+  url.searchParams.set('x', timestamp);
 
   const response = await fetch(url);
 
