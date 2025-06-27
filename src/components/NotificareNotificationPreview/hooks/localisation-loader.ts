817b5b4b0ac9b5d2b7ee2568a6fc7df249510013
rubensantos <rubensantos994@hotmail.com>
2025-06-27 12:00:30 +0100

    refactor: localisationLoader hook adjustments

--- src/components/NotificareNotificationPreview/hooks/localisation-loader.ts
+++ src/components/NotificareNotificationPreview/hooks/localisation-loader.ts
@@ -1,55 +1,63 @@
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { RequestState } from '~/internal/network/state';
 import { isValidLocale } from '~/internal/utils/locale';
-import { MESSAGES, NotificarePushTranslationKey } from '~/locales/push/en';
+import { NotificarePushTranslationKey } from '~/locales';
+import { MESSAGES } from '~/locales/push/en';
 import { MESSAGES_PT } from '~/locales/push/pt';
 
-const DEFAULT_MESSAGES: Record<string, Record<string, string>> = {
-  'en-GB': MESSAGES,
-  'pt-PT': MESSAGES_PT,
-};
+const SUPPORTED_LOCALES = ['en-GB', 'pt-PT'] as const;
 
-export function useLocalisationLoader({ locale, messages }: MessagesLoaderParams) {
+export function useLocalisationLoader({ locale, messages }: LocalisationLoaderParams) {
   const [state, setState] = useState<LocalisationLoaderState>({ status: 'idle' });
 
-  useMemo(() => {
-    if (locale in DEFAULT_MESSAGES) {
+  const localeMessages = useMemo(() => {
+    const safeLocale = SUPPORTED_LOCALES.find((supportedLocale) => supportedLocale === locale);
+
+    if (safeLocale) {
+      return getMessagesForLocale(safeLocale);
+    }
+
+    return null;
+  }, [locale]);
+
+  useEffect(() => {
+    if (localeMessages) {
       setState({
         status: 'success',
         data: {
           locale: locale,
           messages: {
-            ...DEFAULT_MESSAGES[locale],
+            ...localeMessages,
             ...messages,
           },
         },
       });
-    } else {
-      if (!isValidLocale(locale)) {
-        setState({
-          status: 'error',
-          error: new Error(
-            '→ The locale you chose is invalid. Choose a different one and try again',
-          ),
-        });
-      } else {
-        setState({
-          status: 'success',
-          data: {
-            locale: locale,
-            messages: {
-              ...messages,
-            },
-          },
-        });
-      }
+      return;
     }
-  }, [locale, messages]);
+
+    if (!isValidLocale(locale)) {
+      setState({
+        status: 'error',
+        error: new Error('→ The locale you chose is invalid. Choose a different one and try again'),
+      });
+      return;
+    }
+
+    setState({
+      status: 'success',
+      data: {
+        locale: locale,
+        messages: {
+          ...messages,
+        },
+      },
+    });
+  }, [locale, messages, localeMessages]);
 
   return state;
 }
 
-export type MessagesLoaderParams = {
+export type LocalisationLoaderParams = {
   locale: string;
   messages?: Partial<Record<NotificarePushTranslationKey, string>>;
 };
@@ -58,3 +66,14 @@ export type LocalisationLoaderState = Exclude<
   RequestState<{ locale: string; messages: Partial<Record<NotificarePushTranslationKey, string>> }>,
   { status: 'loading' }
 >;
+
+type SupportedLocales = (typeof SUPPORTED_LOCALES)[number];
+
+function getMessagesForLocale(locale: SupportedLocales) {
+  const messages = {
+    'en-GB': MESSAGES,
+    'pt-PT': MESSAGES_PT,
+  };
+
+  return messages[locale];
+}
